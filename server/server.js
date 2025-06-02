1cc3758712ac44669dc707b73a8e0feaf52758ef
Rajinikanth B <65448606+B-Rajinikanth@users.noreply.github.com>
2025-06-02 14:30:40 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,17 +5,17 @@ import applicantRoutes from './routes/applicant.route.js'
 import authRoutes from './routes/authRoutes.js'
 import cors from 'cors';
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+//import path from 'path';
+//import { fileURLToPath } from 'url';
+//import { dirname } from 'path';
 
 const PORT = process.env.PORT || 5555
 
 dotenv.config()
 const app = express()
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = dirname(__filename);
 
 app.use(express.json())
 
@@ -27,9 +27,9 @@ app.use(cors());
 app.use('/applicants', applicantRoutes)
 app.use('/auth', authRoutes)
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+//app.get('*', (req, res) => {
+//  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//});
 
 app.listen(PORT, () => {
     connectDB();
